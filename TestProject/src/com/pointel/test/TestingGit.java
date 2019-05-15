3c34f87d5c426a04357f47c8ece2def4f6541d25
arockia prabakar <arockiaprabakar@pointelsolutions.com>
2019-05-15 12:25:42 +0530

    initial commit
    Change-Id: I49677242727cfd480dbcf38c8c56ca1e12c2dd85
    Signed-off-by: arockia prabakar <arockiaprabakar@pointelsolutions.com>

---
+++ TestProject/src/com/pointel/test/TestingGit.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.pointel.test;
+
+/**
+ * @author Arockiaprabakar
+ *
+ * May 15, 2019
+ */
+public class TestingGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+
+	}
+
+}
