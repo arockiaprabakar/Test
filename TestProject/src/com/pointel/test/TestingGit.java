0b6bf342862302cd1bfb61a4999a4a2b6e047d14
arockia prabakar <arockia15091990@gmail.com>
2019-11-04 12:14:18 +0530

    welcome to github

--- TestProject/src/com/pointel/test/TestingGit.java
+++ TestProject/src/com/pointel/test/TestingGit.java
@@ -14,7 +14,7 @@ public class TestingGit {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello Git");
+		System.out.println("Hello Git welcome to java");
 
 	}
 
